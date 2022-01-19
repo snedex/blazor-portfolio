8cd736ef9909873773d337d53aef44495f810e0d
Will <69020009+snedex@users.noreply.github.com>
2022-01-19 13:53:34 -0500

    Generating Migrations and adding deps for dotnet-ef

---
+++ DevPortfolioBlazor/ServerAPI/Data/Migrations/20220119185235_InitialMigrations.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerAPI.Data.Migrations
+{
+    public partial class InitialMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ThumbnailPath = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "Name", "ThumbnailPath" },
+                values: new object[] { 1, "This is a description of category 1", "Category 1", "uploads/placeholder.jpg" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "Name", "ThumbnailPath" },
+                values: new object[] { 2, "This is a description of category 2", "Category 2", "uploads/placeholder.jpg" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Description", "Name", "ThumbnailPath" },
+                values: new object[] { 3, "This is a description of category 3", "Category 3", "uploads/placeholder.jpg" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
