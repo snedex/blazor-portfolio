5aba7c043a921224c4cb903b8ecff59b66d8870d
Will <69020009+snedex@users.noreply.github.com>
2022-01-19 13:48:32 -0500

    Wiring up DB conext and connection string

--- DevPortfolioBlazor/ServerAPI/Program.cs
+++ DevPortfolioBlazor/ServerAPI/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ServerAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
