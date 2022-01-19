2ca775d97b42b46cad6de1a163857c7945bf0b71
Will <69020009+snedex@users.noreply.github.com>
2022-01-19 14:03:16 -0500

    Adding API to fetch categories

---
+++ DevPortfolioBlazor/ServerAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerAPI.Data;
+
+namespace ServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private AppDbContext _appDbContext;
+
+        public CategoriesController(AppDbContext dbContext)
+        {
+            _appDbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _appDbContext.Categories.ToListAsync();
+
+            return Ok(categories);
+        }
+    }
+}
