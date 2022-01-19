4d274fb530763e5071b7d5f29758c6c4c81c129c
Will <69020009+snedex@users.noreply.github.com>
2022-01-19 14:17:20 -0500

    Creating DB cache service, moving API port

---
+++ DevPortfolioBlazor/BlazorUI/Services/InMemoryDatabaseCache.cs
@@ -0,0 +1,41 @@
+﻿using BlazorUI.Services;
+using Core.Models;
+using System.Net.Http.Json;
+
+internal sealed class InMemoryDatabaseCache
+{
+    private HttpClient _httpClient;
+
+    public InMemoryDatabaseCache(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    private IList<Category> _categories = null;
+    internal IList<Category> Categories
+    {
+        get { return _categories; }
+        set { 
+            _categories = value;
+            NotifyCategoriesDataChanged();
+        }
+    }
+
+    private bool _fetchingRecords = false;
+
+    internal async Task GetCategoriesAndCache()
+    {
+        if (!_fetchingRecords)
+        {
+            _fetchingRecords = true;
+            _categories = await _httpClient.GetFromJsonAsync<List<Category>>(APIEndpoints.s_categories);
+            _fetchingRecords = false;
+        }
+    }
+
+    //event to subscribe to, to listen for data change
+    internal event Action OnCategoriesDataChanged;
+
+    //Fires the event if there are subscribers 
+    private void NotifyCategoriesDataChanged() => OnCategoriesDataChanged?.Invoke();
+}
