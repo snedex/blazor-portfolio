81aa963451d4b7a92f78f21147a4877ebf073304
Will <69020009+snedex@users.noreply.github.com>
2022-01-22 01:38:06 -0500

    Adding authentication provider for the client

--- DevPortfolioBlazor/BlazorUI/Program.cs
+++ DevPortfolioBlazor/BlazorUI/Program.cs
@@ -1,5 +1,7 @@
+using Blazored.LocalStorage;
 using BlazorUI;
 using BlazorUI.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +11,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddSingleton<InMemoryDatabaseCache>();
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AppAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+    provider.GetRequiredService<AppAuthenticationStateProvider>());
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ DevPortfolioBlazor/BlazorUI/Services/AppAuthenticationStateProvider.cs
@@ -0,0 +1,78 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BlazorUI.Services
+{
+    internal class AppAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorage;
+        private readonly JwtSecurityTokenHandler _tokenHandler = new();
+        internal const string c_LocalStorageKey = "bearerToken";
+
+        //This is where the magic happens
+
+        public AppAuthenticationStateProvider(ILocalStorageService localStorage)
+        {
+            this.localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var savedtoken = await localStorage.GetItemAsStringAsync(c_LocalStorageKey);
+
+                if(string.IsNullOrWhiteSpace(savedtoken))
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+                var token = _tokenHandler.ReadJwtToken(savedtoken);
+                
+                if(token.ValidTo < DateTime.UtcNow)
+                {
+                    await localStorage.RemoveItemAsync(c_LocalStorageKey);  
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+                }
+
+                var claims = ParseClaims(token);
+                var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+
+                return new AuthenticationState(user);
+
+            } catch
+            {
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+        }
+
+        private IList<Claim> ParseClaims(JwtSecurityToken token)
+        {
+            IList<Claim> claims = token.Claims.ToList();
+            
+            //Subject is the username/email
+            claims.Add(new Claim(ClaimTypes.Name, token.Subject));
+
+            return claims;
+        }
+
+        internal async Task SignIn()
+        {
+            var savedtoken = await localStorage.GetItemAsStringAsync(c_LocalStorageKey);
+            var securityToken = _tokenHandler.ReadJwtToken(savedtoken);
+
+            var claims = ParseClaims(securityToken);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+
+            Task<AuthenticationState> authState = Task.FromResult(new AuthenticationState(user));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        internal void SignOut()
+        {
+            var nobody = new ClaimsPrincipal(new ClaimsIdentity());
+            Task<AuthenticationState> authState = Task.FromResult(new AuthenticationState(nobody));
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}
